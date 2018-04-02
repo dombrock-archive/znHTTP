732a73a2617778c75f01e308617e9c8fd9f14342
matdombrock <matdombrock@gmail.com>
2018-04-02 16:18:12 -0700

    Depreciate selfCheck.js

--- local_modules/selfCheck.js
+++ local_modules/selfCheck.js
@@ -1,8 +1,9 @@
+//DEPRECIATED
 exports.check = function selfCheck(req,res,scriptName){
   //make sure no one can see the server script
   if(req.url=="/"+scriptName){
   	console.log('hit self check');
-    res.writeHead(300, {'Content-Type': 'text/html'});
+    res.writeHead(403, {'Content-Type': 'text/html'});
     res.end();
     return true;
   }
